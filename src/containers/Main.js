41fee430b1cb02251669d5f2c22f16a920531135
Manav Sankaran <41876220+ManavBala@users.noreply.github.com>
2025-01-06 01:09:26 +0400

    Update Main.js

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -4,6 +4,7 @@ import Greeting from "./greeting/Greeting";
 import Skills from "./skills/Skills";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
+import Research from "./research/research";
 import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
@@ -56,6 +57,7 @@ const Main = () => {
             <StackProgress />
             <Education />
             <WorkExperience />
+            <Research />
             <Projects />
             <StartupProject />
             <Achievement />
