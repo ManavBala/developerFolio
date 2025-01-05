907d03e2577f7536e68092c305d9a427e0d6f287
Manav Sankaran <41876220+ManavBala@users.noreply.github.com>
2025-01-06 01:33:20 +0400

    Update research.js

--- src/containers/research/research.js
+++ src/containers/research/research.js
@@ -13,7 +13,7 @@ export default function Research() {
         <Fade bottom duration={1000} distance="20px">
           <div className="experience-container" id="workExperience">
             <div>
-              <h1 className="experience-heading">Experiences</h1>
+              <h1 className="experience-heading">Research Work</h1>
               <div className="experience-cards-div">
                 {research.experience.map((card, i) => {
                   return (
